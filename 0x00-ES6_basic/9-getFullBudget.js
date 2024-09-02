d66b5507359ffb7be40280ef087e6e525034fc95
TshidisoMp3 <tshidisomorake18@gmail.com>
2024-09-02 23:40:39 +0200

    ES6_Basics

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -4,8 +4,8 @@ export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
     ...budget,
-    getIncomeInDollars: function (income) { return `$${income}`; },
-    getIncomeInEuros: function (income) { return `€${income} euros`; },
+    getIncomeInDollars(income) { return `$${income}`; },
+    getIncomeInEuros(income) { return `€${income} euros`; },
   };
 
   return fullBudget;
