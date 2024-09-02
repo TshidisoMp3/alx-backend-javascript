4bf5dcfa1f5c213e411a0be5ef29effed2aec4c1
TshidisoMp3 <tshidisomorake18@gmail.com>
2024-09-02 21:48:40 +0200

    ES6_Basics

---
+++ 0x00-ES6_basic/6-string-interpolation.js
@@ -0,0 +1,10 @@
+export default function getSanFranciscoNeighborhoods() {
+    const year = 2017;
+    const budget = {
+        income: '$119,868',
+        gdp: '$154.2 billion',
+        capita: '$178,479'
+    };
+
+    return `As of ${year}, it was the seventh-highest income county in the United States, with a per capita personal income of ${budget.capita}. The city's economy is based on finance, professional services, technology, and tourism. San Francisco has the highest percentage`
+}
