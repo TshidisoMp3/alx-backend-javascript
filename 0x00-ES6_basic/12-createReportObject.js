85392d780e5df0f977151d32c1cdad8efaa8ee16
TshidisoMp3 <tshidisomorake18@gmail.com>
2024-09-05 22:39:27 +0200

    ES6_Basics

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,5 +1,8 @@
 export default function createReportObject(employeesList) {
   return {
-    average: employeesList.reduce((acc, employee) => acc + employee.salary, 0) / employeesList.length,
+    allEmployees: { ...employeesList },
+    getNumberOfDepartments: (employeesList) => {
+      return Object.keys(employeesList).length;
+    },
   };
 }
