77549e82c2a529cb4479fd50a618625058177455
TshidisoMp3 <tshidisomorake18@gmail.com>
2024-09-25 12:54:15 +0200

    ES6_Promises

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,7 +1,9 @@
 export default function getResponseFromAPI(success) {
   const pro = new Promise((resolve, reject) => {
-    if (success) {resolve({ status: 200, body: 'Success' });}
-    else {reject(new Error('The fake API is not working currently'));}
+    if (success) { resolve({ status: 200, body: 'Success' });
+  }
+    else { reject(new Error('The fake API is not working currently'));
+  }
   });
   return pro;
 }
