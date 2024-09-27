681688c9c4374d142f1e8bb875a33ecfe3a0f13f
TshidisoMp3 <tshidisomorake18@gmail.com>
2024-09-27 14:29:38 +0200

    ES6_Promises

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -2,5 +2,5 @@ export default function getResponseFromAPI(promise) {
   return promise
     .then(() =>({ status: 200, body:'success' }))
     .catch(() => Error('The fake API is not working currently'));
-    .finally(() => {console.log('end of the call')});
+    .finally(() => {console.log('Got a response from the API')});
 }
