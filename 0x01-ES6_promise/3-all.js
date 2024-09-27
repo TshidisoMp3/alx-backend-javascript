c3731bf8e7cd9483f1d1d49dfbebd466639f16bc
TshidisoMp3 <tshidisomorake18@gmail.com>
2024-09-27 15:16:55 +0200

    ES6_Promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,10 +3,10 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   const pro = Promise.all([uploadPhoto(), createUser()])
     .then((data) => {
-    const fname = data[1].firstName;
-    const lname = data[1].lastName;
-    const bd = data[0].body;
-    console.log(`${bd} ${fname} ${lname}`);
+      const fname = data[1].firstName;
+      const lname = data[1].lastName;
+      const bd = data[0].body;
+      console.log(`${bd} ${fname} ${lname}`);
     })
     .catch(() => { console.log('Signup system offline'); });
   return pro;
