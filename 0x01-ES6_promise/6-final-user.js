5e475f4137853251ff51e16594808b7a264e5364
TshidisoMp3 <tshidisomorake18@gmail.com>
2024-09-30 14:44:15 +0200

    ES6_Promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,14 +1,14 @@
-import signUpUser from "./4-user-promise";
-import uploadPhoto from "./5-photo-reject";
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, filename) {
   const arrays = [];
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(filename)])
-  .then((value) => {
-    value.forEach((val) => {
-      if (val.status === 'fulfilled') arrays.push({ status: val.status, value: val.value });
-      else arrays.push({ status: val.status, reason: val.reason.message });
+    .then((value) => {
+      value.forEach((val) => {
+        if (val.status === 'fulfilled') arrays.push({ status: val.status, value: val.value });
+        else arrays.push({ status: val.status, reason: val.reason.message });
+      });
+      return arrays;
     });
-    return arrays;
-  });
 }
