0722996f1703617dfdcff134fed46081f14088bd
TshidisoMp3 <tshidisomorake18@gmail.com>
2024-11-26 12:56:41 +0200

    NodeJs_Basics

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,10 +2,10 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('readable', () => {
   const procs = process.stdin.read();
   if (procs !== null) {
-    process.stdout.write(`Your name is: ${procs}`);
+    process.stdout.write(`Your name is: ${procs}\n`);
+    process.stdin.pause();
   }
 });
 process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
-
